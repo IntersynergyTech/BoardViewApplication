6af1548ea79cd34d5424e1e35699bf9213bc2f38
Owen Hamer <owen@floul.dev>
2020-12-29 10:22:58 +0000

    Initial commit

---
+++ BoardViewApplication/Pages/Index.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace BoardViewApplication.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+            var client = new HttpClient();
+            var response = client.GetAsync("https://cards.floul.dev/api/Player").Result;
+            var asd1 = response.Content.ReadAsStringAsync().Result;
+            var results = JsonConvert.DeserializeObject<List<Root>>(asd1);
+            asd = results;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public List<Root> asd;
+    }
+    public class Root    {
+        public int playerId { get; set; } 
+        public string userName { get; set; } 
+        public string emailAddress { get; set; } 
+        public string realName { get; set; } 
+        public string password { get; set; } 
+        public DateTime? lastPaid { get; set; } 
+        public bool hasAdminPermission { get; set; } 
+        public bool archived { get; set; } 
+        public DateTime? archiveTime { get; set; } 
+        public decimal currentPosition { get; set; } 
+        public decimal potentialPosition { get; set; }
+
+    }
+}
---
+++ BoardViewApplication/Startup.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace BoardViewApplication
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                    builder =>
+                    {
+                        builder.WithOrigins("https://board.floul.dev",
+                            "https://cards.floul.dev","http://board.floul.dev");
+                    });
+            });
+            services.AddResponseCompression(options =>
+            {
+                options.Providers.Add<GzipCompressionProvider>();
+            });
+            services.AddMvc().AddNewtonsoftJson();
+            services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        { 
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseResponseCompression();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCors(MyAllowSpecificOrigins);
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapRazorPages(); });
+        }
+    }
+}
